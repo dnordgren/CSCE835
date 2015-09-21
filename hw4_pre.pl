d74a23e864d65e036b1073cc2cfeea90b79fd1d9
Jiang Shu <hi.petershu@gmail.com>
2015-09-21 15:07:17 -0500

    add hw4 pre script for DAG on condor.

---
+++ hw4_pre.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl -w
+#use Getopt::Long;
+
+&go ();
+
+sub go {
+    my $start = time;	
+    my $f = "$ARGV[0]"; # change it when use for a diff file.
+    my $ex = $ARGV[1];	
+    my $n = `wc -l $f | perl -pe 's/^ +//g;chomp'`;
+    my ($sum , $x) = split(/\ /,$n);
+    my $node = 12;
+
+    my $line = int($sum/$node);
+    my $last = $sum - ($node - 1) * $line; 
+    print "Splitting the input for parallelization ...\n";
+    &partition($line, $last, $node, $f);
+    &run($ex, $node, $start);
+}
+
+sub partition {
+   my ($line, $last, $node, $f) = (@_);
+   my $last_file_num = $node - 1;
+   my $suffix = "";
+   if($last_file_num < 10){
+     $suffix = "0".$last_file_num;
+   } else {
+     $suffix = $last_file_num;
+   }
+  `split -l $line -a 2 -d $f input_`;
+   my $x = ""; 
+   if($node > 8){
+      $x = $last_file_num + 1;
+   }else{
+     $x = "0".($last_file_num + 1);
+	}
+   `cat input_$x >> input_$last_file_num`;
+   `rm input_$x`;
+}
+
+sub run{
+  my ($ex, $node, $start) = (@_);
+  my $suffix = "";
+  my $dir_num = $node - 1;
+  for (my $i = 0; $i <= $dir_num; $i++){
+      `mkdir -p "job_$i"`;
+      if ($i < 10){
+        $suffix = "0".$i;
+      }else{
+        $suffix = "$i";
+      }
+      `mv input_$suffix ./job_$i/input`
+  }
+}
+

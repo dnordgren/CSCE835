1ead60a118fb69d4d6907fceb08e9f90a1f7b4b5
pi <pi@pi.pi>
2015-11-18 22:44:50 -0600

    version 1 with no removal

---
+++ hw9/part1/WordCount1_unremoval.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount1 {
+
+    public static class TokenizerMapper
+		  extends Mapper<Object, Text, Text, IntWritable> {
+			  private final static IntWritable one = new IntWritable(1);
+			  private Text word = new Text();
+
+			  public void map(Object key, Text value, Context context) 
+			  throws IOException, InterruptedException {
+				StringTokenizer itr = new StringTokenizer(value.toString());
+				  while (itr.hasMoreTokens()) 
+                  {
+                      String myWord = itr.nextToken();
+					  word.set(myWord);
+					  context.write(word, one);
+				  }
+			  }
+		  }
+
+       public static class CombReducer
+           extends Reducer<Text, IntWritable, Text, IntWritable> {
+               private IntWritable result = new IntWritable();
+
+               public void reduce(Text key, Iterable<IntWritable> values, Context context)
+               throws IOException, InterruptedException {
+                  int sum = 0;
+                  for (IntWritable val : values) {
+                       sum += val.get();
+                   }
+                   result.set(sum);
+                   context.write(key, result);
+               }
+         }
+
+	  public static class IntSumReducer
+		  extends Reducer<Text, IntWritable, Text, IntWritable> {
+			  private IntWritable result = new IntWritable();
+
+			  public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+			  throws IOException, InterruptedException {
+				  int sum = 0;
+				  for (IntWritable val : values) {
+					  sum += val.get();
+				  }
+				  result.set(sum);
+				  // filtering
+				  if(sum > 10){
+				    context.write(key, result);
+			    }
+			  }
+		}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job wcjob = Job.getInstance(conf, "word count");
+		wcjob.setJarByClass(WordCount1.class);
+		wcjob.setMapperClass(TokenizerMapper.class);
+		wcjob.setCombinerClass(CombReducer.class);
+		wcjob.setReducerClass(IntSumReducer.class);
+		wcjob.setOutputKeyClass(Text.class);
+		wcjob.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(wcjob, new Path(args[0]));
+		FileOutputFormat.setOutputPath(wcjob, new Path(args[1]));
+		
+		System.exit(wcjob.waitForCompletion(true) ? 0 : 1);
+	}
+}
